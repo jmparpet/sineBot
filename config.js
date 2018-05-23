f795dfce5cfaa59d651018ef13b54f64e6319a5a
Jean-Marie Parpet <jmparpet@me.com>
2018-05-23 18:01:26 +0200

    Remove API keys

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         '8QevAHIq5GAwhv6aFzbU1lpw7',
-  consumer_secret:      '8U6VuHywCK0DPsaR5zEphbi6bZAAM7kI5xwNLO7pxH2y6tNleC',
-  access_token:         '994185294864965633-GJlV6K25NZKoRKNEobkHXm2UmOd8FeO',
-  access_token_secret:  'ph1orIfTJv24t5f3hmR84pm2c8wv5YI72i5fXUHrpo1Ap'
-}
